e4fd88654496b527fe48b3f50afef199329f0004
Ryan Brown <rkbrown25@meta.com>
2023-07-03 16:19:53 -0700

    Created css for AddConstraint component that will add constraints and modified Constraints jsx and css to add header

---
+++ advisor-ui/src/components/AddConstraint/AddConstraint.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./AddConstraint.css";
+
+export default function AddConstraint() {
+  return (
+    <div className="addconstraint">
+      <p>Add Constraint</p>
+    </div>
+  );
+}
--- advisor-ui/src/components/Constraints/Constraints.jsx
+++ advisor-ui/src/components/Constraints/Constraints.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import "./Constraints.css";
 
 import ConstraintItem from "../ConstraintItem/ConstraintItem";
+import AddConstraint from "../AddConstraint/AddConstraint";
 
 export default function Constraints() {
   return (
     <div className="constraints">
-      <ConstraintItem type="Class" value="Calculus 2" />
-      <ConstraintItem type="Class" value="Calculus 2" />
+      <h3>Constraints:</h3>
+      <div className="content">
+        <ConstraintItem type="Class" value="Calculus 2" />
+        <ConstraintItem type="Class" value="Calculus 2" />
+        <AddConstraint />
+      </div>
     </div>
   );
 }
