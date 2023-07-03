f5d564b5d21d7b791f364e9257c543324a624444
Ryan Brown <rkbrown25@meta.com>
2023-07-03 14:26:07 -0700

    Created InputForm component for reusable input across app and added state to handle inputs in Login page component

--- advisor-ui/src/pages/Login/Login.jsx
+++ advisor-ui/src/pages/Login/Login.jsx
@@ -2,11 +2,34 @@ import * as React from "react";
 import { useEffect, useState } from "react";
 
 import "./Login.css";
+import InputForm from "../../components/InputForm/InputForm";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div className="login">
-      <p>Login</p>
+      <InputForm
+        value={email}
+        handleChange={handleChangeEmail}
+        type="text"
+        placeholder="Enter email"
+      />
+      <InputForm
+        value={password}
+        handleChange={handleChangePassword}
+        type="password"
+        placeholder="Enter password"
+      />
     </div>
   );
 }
