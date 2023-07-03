8afc95415b4be47aaccd690d71b02405b708c0d5
Ryan Brown <rkbrown25@meta.com>
2023-07-03 11:45:51 -0700

    Removed default css styles, created basic react router setup, and init LandingPage component with boilerplate

---
+++ advisor-ui/src/pages/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { useEffect, useState } from "react";
+
+import "./Landing.css";
+
+export default function Landing() {
+  return (
+    <div className="landing">
+      <p>Hi!</p>
+    </div>
+  );
+}
