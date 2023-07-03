d9203c4e4d1f8e2c76219151b1fe0b1ef481886e
Ryan Brown <rkbrown25@meta.com>
2023-07-03 15:11:55 -0700

    Added header and navigation buttons to Student Landing page and adjusted layout

--- advisor-ui/src/pages/StudentLanding/StudentLanding.jsx
+++ advisor-ui/src/pages/StudentLanding/StudentLanding.jsx
@@ -1,12 +1,21 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 import "./StudentLanding.css";
 
 export default function StudentLanding() {
   return (
     <div className="studentlanding">
-      <p>Student Landing</p>
+      <h1>Welcome!</h1>
+      <div className="buttons">
+        <Link to="/student/schedule">
+          <button>Course Schedule Tool</button>
+        </Link>
+        <Link to="/student/volunteer">
+          <button>Volunteer Tool</button>
+        </Link>
+      </div>
     </div>
   );
 }
