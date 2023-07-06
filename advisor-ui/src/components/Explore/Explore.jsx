fc2dc7dbcfe3576add221957a8a57b0dd27adcce
Ryan Brown <rkbrown25@meta.com>
2023-07-05 17:10:30 -0700

    Added filler photo for opportuntiy card components, and completed basic layout and css of Explore component

--- advisor-ui/src/components/Explore/Explore.jsx
+++ advisor-ui/src/components/Explore/Explore.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Explore.css";
+import OpportunityCard from "../OpportunityCard/OpportunityCard";
 
-export default function Explore() {
+export default function Explore({ events }) {
+  const eventItems = events.map((eventItem) => (
+    <OpportunityCard title={eventItem.title} img={eventItem.img} />
+  ));
   return (
     <div className="explore">
       <h3>Explore All Events:</h3>
-      <div className="content"></div>
+      <div className="content">{eventItems}</div>
     </div>
   );
 }
--- advisor-ui/src/pages/VolunteerExplore/VolunteerExplore.jsx
+++ advisor-ui/src/pages/VolunteerExplore/VolunteerExplore.jsx
@@ -3,12 +3,25 @@ import "./VolunteerExplore.css";
 import Explore from "../../components/Explore/Explore";
 import { Link } from "react-router-dom";
 
+import img from "../../assets/volunteer-opportunities-ideas-article-1200x800.jpg";
+
 export default function VolunteerExplore() {
+  const events = [
+    { title: "Charity Run", img: img },
+    { title: "Charity Feast", img: img },
+    { title: "Feast Run", img: img },
+    { title: "Run Run", img: img },
+    { title: "Cat Run", img: img },
+    { title: "Handy Run", img: img },
+    { title: "Dog Handt", img: img },
+    { title: "Who Run", img: img },
+    { title: "We Run", img: img },
+  ];
   return (
     <div className="volunteerexplore">
       <div className="content">
         <Link to="/student/landing">Back</Link>
-        <Explore />
+        <Explore events={events} />
       </div>
     </div>
   );
