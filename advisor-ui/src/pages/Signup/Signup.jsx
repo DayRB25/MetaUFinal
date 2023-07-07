b5b4313b685b7ec03d6e096f24ffe0a4518c1a3a
Ryan Brown <rkbrown25@meta.com>
2023-07-07 15:21:55 -0700

    InputForm component for each of the required fields for account creation, implemented basic change handlers

--- advisor-ui/src/pages/Signup/Signup.jsx
+++ advisor-ui/src/pages/Signup/Signup.jsx
@@ -1,5 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import InputForm from "../../components/InputForm/InputForm";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [year, setYear] = useState("");
+  const [city, setCity] = useState("");
+  const [locationState, setLocationState] = useState("");
+  const [address, setAddress] = useState("");
+
+  const handleFirstNameChange = (e) => {
+    setFirstname(e.target.value);
+  };
+
+  const handleLastNameChange = (e) => {
+    setLastname(e.target.value);
+  };
+
+  const handleUserNameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleYearChange = (e) => {
+    setYear(e.target.value);
+  };
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
+  };
+  const handleStateChange = (e) => {
+    setLocationState(e.target.value);
+  };
+  const handleAddressChange = (e) => {
+    setAddress(e.target.value);
+  };
+
+  return (
+    <div className="signup">
+      <div className="content">
+        <InputForm
+          type="text"
+          placeholder="First name"
+          value={firstname}
+          handleChange={handleFirstNameChange}
+        />
+        <InputForm
+          type="text"
+          placeholder="Last name"
+          value={lastname}
+          handleChange={handleLastNameChange}
+        />
+        <InputForm
+          type="email"
+          placeholder="Email"
+          value={email}
+          handleChange={handleEmailChange}
+        />
+        <InputForm
+          type="text"
+          placeholder="Username"
+          value={username}
+          handleChange={handleUserNameChange}
+        />
+        <InputForm
+          type="password"
+          placeholder="Password"
+          value={password}
+          handleChange={handlePasswordChange}
+        />
+        <InputForm
+          type="text"
+          placeholder="Year"
+          value={year}
+          handleChange={handleYearChange}
+        />
+        <InputForm
+          type="text"
+          placeholder="City"
+          value={city}
+          handleChange={handleCityChange}
+        />
+        <InputForm
+          type="text"
+          placeholder="State"
+          value={locationState}
+          handleChange={handleStateChange}
+        />
+        <InputForm
+          type="text"
+          placeholder="Address"
+          value={address}
+          handleChange={handleAddressChange}
+        />
+      </div>
+    </div>
+  );
 }
