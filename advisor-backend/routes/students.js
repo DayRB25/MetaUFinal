03624e534c58fafc75c26878e791870975dba91c
Ryan Brown <rkbrown25@meta.com>
2023-07-07 12:59:36 -0700

    Implemented user login route/request, tested in Postman

--- advisor-backend/routes/students.js
+++ advisor-backend/routes/students.js
@@ -5,6 +5,35 @@ import { Op } from "sequelize";
 
 const router = express.Router();
 
+// Route for student login
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Find the student by username
+    const student = await Student.findOne({ where: { username } });
+
+    if (!student) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+
+    // Compare the password
+    const isValidPassword = await bcrypt.compare(password, student.password);
+
+    if (!isValidPassword) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+
+    // Set the student in the session
+    req.session.user = student;
+
+    // Return the student data in the response
+    res.json({ student });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
 // Route for student account creation
 router.post("/create", async (req, res) => {
   const { username, password, email, ...otherFields } = req.body;
