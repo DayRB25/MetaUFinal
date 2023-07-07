3a58e6fc2fb147430980f8118d613c2d786cb41e
Ryan Brown <rkbrown25@meta.com>
2023-07-07 14:14:06 -0700

    Implemented route/request fetch an event by ID
    Implented route/request to fetch events by ID, tested in Postman

--- advisor-backend/routes/events.js
+++ advisor-backend/routes/events.js
@@ -12,4 +12,19 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:eventId", async (req, res) => {
+  try {
+    const eventId = req.params.eventId;
+    const event = await EventDetails.findByPk(eventId);
+    if (!event) {
+      res.status(404).json({ error: "Resource not found" });
+      return;
+    } else {
+      res.json({ event });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
 export default router;
