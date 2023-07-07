a4378b1d981df39bb0e03a262e7b0ee937cb473e
Ryan Brown <rkbrown25@meta.com>
2023-07-07 12:27:25 -0700

    Importing libraries, binding middleware to app, enable session storage in SQL DB and sync models, and start express server

---
+++ advisor-backend/server.js
@@ -0,0 +1,55 @@
+const port = process.env.PORT || 5000;
+import express from "express";
+import session from "express-session";
+import cors from "cors";
+import morgan from "morgan";
+import "dotenv/config";
+import { sequelize } from "./database.js";
+import { Student } from "./models/index.js";
+import studentRoute from "./routes/students.js";
+import SequelizeStoreInit from "connect-session-sequelize";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json()); // Middleware for parsing JSON bodies from HTTP requests
+app.use(morgan());
+
+const SequelizeStore = SequelizeStoreInit(session.Store);
+const sessionStore = new SequelizeStore({
+  db: sequelize,
+});
+
+// Session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      sameSite: false,
+      secure: false,
+      expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000), // 1 year in milliseconds
+    },
+  })
+);
+sessionStore.sync();
+
+app.use("/api/student", studentRoute);
+
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App is listening on PORT: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Unable to connet to the database", error);
+  });
