f28c05ed9b9235215a018d55994b43367f9188b6
Ryan Brown <rkbrown25@meta.com>
2023-07-11 17:08:33 -0700

    Added material UI Icons to Opportuntiy Modal for description of properties in modal, adjust JSX structure for future styling

--- advisor-ui/src/components/OpportunityModal/OpportunityModal.jsx
+++ advisor-ui/src/components/OpportunityModal/OpportunityModal.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./OpportunityModal.css";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import CloseIcon from "@mui/icons-material/Close";
+import InfoIcon from "@mui/icons-material/Info";
 
 export default function OpportunityModal({ eventItem, handleCloseModal }) {
   // YYYY-MM-DD = 10 chars
@@ -8,18 +13,25 @@ export default function OpportunityModal({ eventItem, handleCloseModal }) {
   return (
     <div className="oppmodal">
       <div className="content">
-        <button onClick={handleCloseModal}>Back</button>
+        <CloseIcon className="close" onClick={handleCloseModal} />
         <h3>{eventItem.title}</h3>
         <div className="location">
+          <LocationOnIcon />
           <p>{`${eventItem.city}, ${eventItem.state}`}</p>
         </div>
         <div className="date">
+          <CalendarMonthIcon />
           <p>{eventItem.date.slice(0, standardDateLength)}</p>
         </div>
         <div className="admin">
+          <SupervisorAccountIcon />
           <p>{eventItem.admin}</p>
         </div>
         <div className="description">
+          <div className="icon-title">
+            <InfoIcon />
+            <p>Description</p>
+          </div>
           <p>{eventItem.description}</p>
         </div>
       </div>
