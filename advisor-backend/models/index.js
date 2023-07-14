82de3a87beec2407177595167378c8e2bad5e5a0
Ryan Brown <rkbrown25@meta.com>
2023-07-14 10:34:54 -0700

    Defining many to many relationship between EventDetail and Student through StudentEvent

--- advisor-backend/models/index.js
+++ advisor-backend/models/index.js
@@ -1,3 +1,12 @@
 import { Student } from "./student.js";
 import { EventDetails } from "./event.js";
-export { Student, EventDetails };
+import { StudentEvent } from "./studentevent.js";
+
+EventDetails.belongsToMany(Student, {
+  through: StudentEvent,
+});
+Student.belongsToMany(EventDetails, {
+  through: StudentEvent,
+});
+
+export { Student, EventDetails, StudentEvent };
