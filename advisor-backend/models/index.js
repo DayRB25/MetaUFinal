d70e5e9ba0afde6363d1dca18a7c719313bda1df
Ryan Brown <rkbrown25@meta.com>
2023-07-14 14:27:01 -0700

    Adjusted relationship definition between StudentEvent, EventDetails, and Student entities

--- advisor-backend/models/index.js
+++ advisor-backend/models/index.js
@@ -2,11 +2,7 @@ import { Student } from "./student.js";
 import { EventDetails } from "./event.js";
 import { StudentEvent } from "./studentevent.js";
 
-EventDetails.belongsToMany(Student, {
-  through: StudentEvent,
-});
-Student.belongsToMany(EventDetails, {
-  through: StudentEvent,
-});
+StudentEvent.belongsTo(Student);
+StudentEvent.belongsTo(EventDetails);
 
 export { Student, EventDetails, StudentEvent };
