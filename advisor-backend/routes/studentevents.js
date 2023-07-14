7a8f1e2f6592b8d0dff1d4c55805521e482a2279
Ryan Brown <rkbrown25@meta.com>
2023-07-14 14:49:16 -0700

    Route and request handling to create StudentEvent entities in DB, tested with Postman

--- advisor-backend/routes/studentevents.js
+++ advisor-backend/routes/studentevents.js
@@ -5,4 +5,40 @@ import { StudentEvent } from "../models/index.js";
 
 const router = express.Router();
 
+// Route for student event creation
+router.post("/create", async (req, res) => {
+  const StudentId = req.body.studentId;
+  const EventDetailId = req.body.eventDetailId;
+  const hours = req.body.hours;
+
+  try {
+    // Check if student with id exists
+    const student = await Student.findOne({ where: { id: StudentId } });
+
+    if (!student) {
+      return res.status(400).json({ error: "Student does not exist" });
+    }
+    // Check if the event with id exists
+    const eventDetail = await EventDetails.findOne({
+      where: { id: EventDetailId },
+    });
+    if (!eventDetail) {
+      return res.status(400).json({ error: "Event does not exist" });
+    }
+
+    // create student event
+    const newStudentEvent = await StudentEvent.create({
+      StudentId,
+      EventDetailId,
+      hours,
+    });
+
+    // Return the student data in the response
+    res.json({ newStudentEvent });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
 export default router;
