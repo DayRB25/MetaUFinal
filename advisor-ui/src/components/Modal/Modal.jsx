696d8f0a49a6d9d95086f1a7278d144ce80ab971
Ryan Brown <rkbrown25@meta.com>
2023-07-17 12:22:42 -0700

    Basic JSX structure and CSS for general Modal Component

---
+++ advisor-ui/src/components/Modal/Modal.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Modal as ModalUI } from "@mui/material";
+import Box from "@mui/material/Box";
+import CloseIcon from "@mui/icons-material/Close";
+import "./Modal.css";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  borderRadius: 2,
+  p: 4,
+};
+
+export default function Modal({ isOpen, content, handleCloseModal }) {
+  return (
+    <ModalUI open={isOpen} disableAutoFocus={true}>
+      <Box sx={style}>
+        <div className="close-container">
+          <CloseIcon className="close" onClick={handleCloseModal} />
+        </div>
+        {content}
+      </Box>
+    </ModalUI>
+  );
+}
