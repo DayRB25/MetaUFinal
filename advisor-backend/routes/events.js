02669fbec792d72986401149fe76464a8a9b259d
Ryan Brown <rkbrown25@meta.com>
2023-07-17 13:26:58 -0700

    Added route and request to calculate the number of pages needed to display content based on the number of records in the DB and the current pageLimit

--- advisor-backend/routes/events.js
+++ advisor-backend/routes/events.js
@@ -3,6 +3,16 @@ import { EventDetails } from "../models/event.js";
 const pageLimit = 8;
 const router = express.Router();
 
+router.get("/page-count", async (req, res) => {
+  try {
+    const count = await EventDetails.count();
+    const pageCount = Math.ceil(count / pageLimit);
+    res.status(200).json({ pageCount });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
 router.get("/:page", async (req, res) => {
   const page = req.params.page;
   try {
