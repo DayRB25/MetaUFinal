d46d2ebb7f7b4a1bac3dc79f3b866bd943a9ff9d
Ryan Brown <rkbrown25@meta.com>
2023-07-17 10:24:25 -0700

    Added MUI Btn and InputForm to collect number of hours for StudentEvent creation, setup hours state and handler to update state

--- advisor-ui/src/components/OpportunityModal/OpportunityModal.jsx
+++ advisor-ui/src/components/OpportunityModal/OpportunityModal.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./OpportunityModal.css";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
 import CloseIcon from "@mui/icons-material/Close";
 import InfoIcon from "@mui/icons-material/Info";
+import InputForm from "../InputForm/InputForm";
+import { Button } from "@mui/material";
 
 export default function OpportunityModal({ eventItem, handleCloseModal }) {
   // YYYY-MM-DD = 10 chars
   const standardDateLength = 10;
+  const [hours, setHours] = useState("");
+  const handleChangeHours = (e) => {
+    setHours(e.target.value);
+  };
 
   return (
     <div className="oppmodal">
@@ -33,6 +39,15 @@ export default function OpportunityModal({ eventItem, handleCloseModal }) {
             <p>Description</p>
           </div>
           <p>{eventItem.description}</p>
+        </div>
+        <div className="add-student-event">
+          <InputForm
+            type="text"
+            placeholder="Enter number of hours"
+            value={hours}
+            handleChange={handleChangeHours}
+          />
+          <Button variant="outlined">Attended Event</Button>
         </div>
       </div>
     </div>
