25b1635321ea698b36bbf4e2160bf3b757cfa179
Ryan Brown <rkbrown25@meta.com>
2023-07-17 15:53:45 -0700

    Small change for case of inner text, handled with CSS property instead

--- advisor-ui/src/components/AddConstraint/AddConstraint.jsx
+++ advisor-ui/src/components/AddConstraint/AddConstraint.jsx
@@ -5,7 +5,7 @@ import { Button } from "@mui/material";
 export default function AddConstraint({ openPopover }) {
   return (
     <Button variant="outlined" className="add-constraint" onClick={openPopover}>
-      ADD CONSTRAINT
+      add constraint
     </Button>
   );
 }
