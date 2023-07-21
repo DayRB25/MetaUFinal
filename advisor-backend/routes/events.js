35b1a8c8ff84747885cc24d15e6f9810cbd0b60e
Ryan Brown <rkbrown25@meta.com>
2023-07-20 17:09:36 -0700

    Added basic similarity metric for start time such that if start time of event is within an hour of either end of the preferred start time range, a point will be awarded

--- advisor-backend/routes/events.js
+++ advisor-backend/routes/events.js
@@ -133,6 +133,8 @@ router.get("/recommended", async (req, res) => {
         END AS location_score,
         CASE
           WHEN (time >= '${req.query.start_time}' AND time < '${req.query.end_time}') THEN 2
+          WHEN (time < '${req.query.start_time}' AND '${req.query.start_time}' - time <= interval '1 hour') THEN 1
+          WHEN (time >= '${req.query.end_time}' AND time - '${req.query.end_time}' <= interval '1 hour') THEN 1
           ELSE 0
         END AS starttime_score,
         CASE
