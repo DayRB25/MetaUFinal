fa0424b357fea9d49c85b8cb623804f794ab01f1
Ryan Brown <rkbrown25@meta.com>
2023-07-24 14:47:36 -0700

    Displaying strength of match tag with a MUI Chip component

--- advisor-ui/src/components/OpportunityCard/OpportunityCard.jsx
+++ advisor-ui/src/components/OpportunityCard/OpportunityCard.jsx
@@ -3,9 +3,18 @@ import defaultImg from "../../assets/volunteer-opportunities-ideas-article-1200x
 import "./OpportunityCard.css";
 import OpportunityModal from "../OpportunityModal/OpportunityModal";
 import Modal from "../Modal/Modal";
+import Chip from "@mui/material/Chip";
+import Grow from "@mui/material/Grow";
 
 export default function OpportunityCard({ eventItem }) {
   const [isOpen, setIsOpen] = useState(false);
+
+  let backgroundColor = "#8BC34A";
+  if (eventItem.match && eventItem.match === "medium") {
+    backgroundColor = "#FFD54F";
+  } else if (eventItem.match && eventItem.match === "low") {
+    backgroundColor = "#E57373";
+  }
 
   const handleOpenModal = () => {
     setIsOpen(true);
@@ -17,6 +26,25 @@ export default function OpportunityCard({ eventItem }) {
 
   return (
     <div className="opportunity-card">
+      {eventItem.match && (
+        <Grow in={true} timeout={500}>
+          <Chip
+            label={eventItem.match}
+            color="primary"
+            style={{
+              backgroundColor,
+              boxShadow: "rgba(100, 100, 111, 0.3) 0px 7px 29px 0px",
+              borderRadius: "5px",
+              color: "black",
+              fontWeight: "600",
+              position: "absolute",
+              right: 5,
+              textTransform: "uppercase",
+              top: 5,
+            }}
+          />
+        </Grow>
+      )}
       <div className="content" onClick={handleOpenModal}>
         <img src={eventItem.img ?? defaultImg} alt="event cover" />
         <p>{eventItem.title}</p>
