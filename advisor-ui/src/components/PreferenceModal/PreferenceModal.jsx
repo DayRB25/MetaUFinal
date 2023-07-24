8ab0a9d2f3ee1b055a1fab193096d4768b4ee6b6
Ryan Brown <rkbrown25@meta.com>
2023-07-24 12:43:06 -0700

    Import packages needed for Date/Time Pickers and create div for date range pickers

--- advisor-ui/src/components/PreferenceModal/PreferenceModal.jsx
+++ advisor-ui/src/components/PreferenceModal/PreferenceModal.jsx
@@ -3,24 +3,36 @@ import "./PreferenceModal.css";
 import Slider from "@mui/material/Slider";
 import { Button } from "@mui/material";
 import { distanceMarks } from "./distanceMarks";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 export default function PreferenceModal() {
   return (
-    <div className="preference-modal">
-      <div className="content">
-        <h4>Preferences:</h4>
-        <div className="distance">
-          <p>Distance: (km)</p>
-          <Slider
-            step={10}
-            min={10}
-            max={100}
-            marks={distanceMarks}
-            valueLabelDisplay="auto"
-          />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div className="preference-modal">
+        <div className="content">
+          <h4>Preferences:</h4>
+          <div className="distance">
+            <p>Distance: (km)</p>
+            <Slider
+              step={10}
+              min={10}
+              max={100}
+              marks={distanceMarks}
+              valueLabelDisplay="auto"
+            />
+          </div>
+          <div className="date-range">
+            <p>Date Range:</p>
+            <div className="date-pickers">
+              <DatePicker label="Start Of Window" />
+              <DatePicker label="End Of Window" />
+            </div>
+          </div>
+          <Button variant="outlined">Submit</Button>
         </div>
-        <Button variant="outlined">Submit</Button>
       </div>
-    </div>
+    </LocalizationProvider>
   );
 }
