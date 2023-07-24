1f8312cd6e78a47eaf40a3e0920dcad48c6a2705
Ryan Brown <rkbrown25@meta.com>
2023-07-24 12:37:31 -0700

    Install packages for MUI date/time pickers, basic slider for distance preference and distanceMarks file for display labels

--- advisor-ui/src/components/PreferenceModal/PreferenceModal.jsx
+++ advisor-ui/src/components/PreferenceModal/PreferenceModal.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import "./PreferenceModal.css";
+import Slider from "@mui/material/Slider";
+import { Button } from "@mui/material";
+import { distanceMarks } from "./distanceMarks";
 
 export default function PreferenceModal() {
-  return <div>PreferenceModal</div>;
+  return (
+    <div className="preference-modal">
+      <div className="content">
+        <h4>Preferences:</h4>
+        <div className="distance">
+          <p>Distance: (km)</p>
+          <Slider
+            step={10}
+            min={10}
+            max={100}
+            marks={distanceMarks}
+            valueLabelDisplay="auto"
+          />
+        </div>
+        <Button variant="outlined">Submit</Button>
+      </div>
+    </div>
+  );
 }
