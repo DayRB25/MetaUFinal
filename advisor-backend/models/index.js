7080c90ed2461fb47a2eaf3bfe7a46c9d67a17a7
Ryan Brown <rkbrown25@meta.com>
2023-07-26 13:05:17 -0700

    Defining relationships to be used in the course schedule algo

--- advisor-backend/models/index.js
+++ advisor-backend/models/index.js
@@ -2,6 +2,22 @@ import { Student } from "./student.js";
 import { EventDetail } from "./event.js";
 import { StudentEvent } from "./studentevent.js";
 import { StudentSignup } from "./studentsignup.js";
+import { School } from "./school.js";
+import { Class } from "./class.js";
+import { RequiredClass } from "./requiredclass.js";
+import { Prerequisite } from "./prerequisite.js";
+import { TakenClass } from "./takenclass.js";
+
+Prerequisite.belongsTo(Class, { foreignKey: "PostreqId" });
+Prerequisite.belongsTo(Class, { foreignKey: "PrereqId" });
+
+TakenClass.belongsTo(Student);
+TakenClass.belongsTo(Class);
+
+Class.belongsTo(School);
+RequiredClass.belongsTo(School);
+
+Student.belongsTo(School);
 
 StudentEvent.belongsTo(Student);
 StudentEvent.belongsTo(EventDetail);
@@ -9,4 +25,14 @@ StudentEvent.belongsTo(EventDetail);
 StudentSignup.belongsTo(Student);
 StudentSignup.belongsTo(EventDetail);
 
-export { Student, EventDetail, StudentEvent, StudentSignup };
+export {
+  Student,
+  EventDetail,
+  StudentEvent,
+  StudentSignup,
+  Prerequisite,
+  TakenClass,
+  Class,
+  RequiredClass,
+  School,
+};
