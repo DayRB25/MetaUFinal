124f142444d9fbf4c78acbb217ca7f572e84595d
Ryan Brown <rkbrown25@meta.com>
2023-07-27 15:58:56 -0700

    Implemented route to fetch static map (from Bing API) using latitude and longitude passed in as params, encode binary data in base 64 string and prepend encoding header

--- advisor-backend/routes/maps.js
+++ advisor-backend/routes/maps.js
@@ -1,5 +1,31 @@
 import express from "express";
+import axios from "axios";
 
 const router = express.Router();
+
+// encode image into base 64
+function image_encode(data) {
+  return new Buffer(data).toString("base64");
+}
+
+// Route for fetching map from BingAPI
+router.get("/:latitude/:longitude", async (req, res) => {
+  const latitude = req.params.latitude;
+  const longitude = req.params.longitude;
+
+  try {
+    const response = await axios.get(
+      `https://dev.virtualearth.net/REST/V1/Imagery/Map/Road/${latitude},${longitude}/16?mapSize=600%2C600&format=png&pushpin=${latitude},${longitude};64&key=Apa9ksJ4enTFivZjQC-1l-yGxIf1aJCY1VBPWmrFlFaF9eLs2VUlNh2TyG4DaMi_`,
+      {
+        responseType: "arraybuffer",
+      }
+    );
+    res.json({
+      response: "data:image/png;base64," + image_encode(response.data),
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 export default router;
