0fc753abf83442e4adb6ce1917eff5667cee46c6
Ryan Brown <rkbrown25@meta.com>
2023-07-31 15:29:58 -0700

    Basic Layout and elements for a non-full year year-swap

--- advisor-ui/src/components/SwapModal/SwapModal.jsx
+++ advisor-ui/src/components/SwapModal/SwapModal.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { Button } from "@mui/material";
+import { FormControl } from "@mui/material";
+import { Select } from "@mui/material";
+import { InputLabel } from "@mui/material";
 
 export default function SwapModal({ courseName }) {
-  return <div>{courseName}</div>;
+  return (
+    <div className="swap-modal">
+      <div className="content">
+        <h4>Year Swap:</h4>
+        <p>{`What year do you want to move ${courseName} to?`}</p>
+        <FormControl sx={{ minWidth: 120 }} size="small">
+          <InputLabel id="select-label">Year</InputLabel>
+          <Select
+            labelId="select-label"
+            id="simple-select"
+            value=""
+            label="Year"
+          ></Select>
+        </FormControl>
+        <Button variant="outlined">Submit</Button>
+      </div>
+    </div>
+  );
 }
