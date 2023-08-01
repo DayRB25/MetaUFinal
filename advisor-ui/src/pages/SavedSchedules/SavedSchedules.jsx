a69c1571efe853b5a893d135a078eda4ce886202
Ryan Brown <rkbrown25@meta.com>
2023-08-01 02:55:03 -0700

    Basic element layout and JSX for save schedule page

--- advisor-ui/src/pages/SavedSchedules/SavedSchedules.jsx
+++ advisor-ui/src/pages/SavedSchedules/SavedSchedules.jsx
@@ -1,3 +1,23 @@
+import { Pagination } from "@mui/material";
+import { Link } from "react-router-dom";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+
 export default function SavedSchedules() {
-  return <div className="saved-schedules">saved schedules</div>;
+  return (
+    <div className="saved-schedules">
+      <div className="content">
+        <div className="back-btn">
+          <Link to="/student/schedule">
+            <ArrowBackIcon className="back" />
+          </Link>
+        </div>
+        <div className="header">
+          <h3>Saved Schedules:</h3>
+        </div>
+        <div className="pagination">
+          <Pagination count={2} />
+        </div>
+      </div>
+    </div>
+  );
 }
