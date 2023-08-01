a724dfbfe57d723940cc547b185e758bb47e161c
Ryan Brown <rkbrown25@meta.com>
2023-07-31 23:15:04 -0700

    Schema model for admin

---
+++ advisor-backend/models/admin.js
@@ -0,0 +1,37 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../database.js";
+
+export const Admin = sequelize.define("Admin", {
+  firstname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  state: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
