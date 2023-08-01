9970a5a5b9a854a42582ac5b9486eb6a96a8df4c
Ryan Brown <rkbrown25@meta.com>
2023-08-01 02:06:59 -0700

    Implemented endpoint to support paginated queries returning schedule object, for each schedule included in the current page, builds a schedule array and pushes it to the overall return array

--- advisor-backend/routes/save-schedule.js
+++ advisor-backend/routes/save-schedule.js
@@ -3,6 +3,7 @@ import { Schedule } from "../models/index.js";
 import { Year } from "../models/index.js";
 import { Semester } from "../models/index.js";
 import { SemesterClass } from "../models/index.js";
+import { Class } from "../models/index.js";
 
 const pageLimit = 1;
 
@@ -61,6 +62,81 @@ router.get("/:studentId/page-count", async (req, res) => {
     res.status(200).json({ pageCount });
   } catch (error) {
     res.status(500).json({ error: "Server error" });
+  }
+});
+
+// fetch schedules associated with provided studentId
+router.get("/:studentId/:page", async (req, res) => {
+  const StudentId = req.params.studentId;
+  const page = req.params.page;
+  const schedulesArray = [];
+
+  try {
+    const schedules = await Schedule.findAll({
+      where: {
+        StudentId,
+      },
+      limit: pageLimit,
+      offset: (page - 1) * pageLimit,
+    });
+
+    const schedulesDataIDs = schedules.map(
+      (schedule) => schedule.dataValues.id
+    );
+
+    for (let i = 0; i < schedulesDataIDs.length; i++) {
+      const ScheduleId = schedulesDataIDs[i];
+      const schedule = [];
+      const years = await Year.findAll({ where: { ScheduleId } });
+      // check if years is empty, if its empty, no ScheduleId
+      if (years.length === 0) {
+        return res.status(400).json({ message: "Schedule does not exist" });
+      }
+
+      for (let i = 0; i < years.length; i++) {
+        const year = years[i];
+        const semesters = [];
+
+        const yearSemesters = await Semester.findAll({
+          where: { YearId: year.id },
+        });
+
+        for (let j = 0; j < yearSemesters.length; j++) {
+          const semester = yearSemesters[j];
+          const classes = [];
+
+          const semesterClasses = await SemesterClass.findAll({
+            where: { SemesterId: semester.id },
+          });
+          for (let k = 0; k < semesterClasses.length; k++) {
+            const classId = semesterClasses[k].ClassId;
+            const classItem = await Class.findOne({ where: { id: classId } });
+
+            // create class object and add to classes array
+            const classObject = {
+              id: classId,
+              description: classItem.description,
+              units: classItem.units,
+              name: classItem.name,
+            };
+            classes.push(classObject);
+          }
+          // create semester object and add to semesters array
+          const semesterObject = { number: semester.number, classes: classes };
+          semesters.push(semesterObject);
+        }
+        // create year objects and add to schedule array
+        const yearObject = { number: year.number, semesters: semesters };
+        schedule.push(yearObject);
+      }
+      schedulesArray.push(schedule);
+    }
+
+    res.status(200).json({ schedules: schedulesArray });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Fetch unsuccessful. Internal server error" });
   }
 });
 
