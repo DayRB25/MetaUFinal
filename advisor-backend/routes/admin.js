179e339453eacaec5506e51e910a17a17d27f4eb
Ryan Brown <rkbrown25@meta.com>
2023-07-31 23:28:31 -0700

    Implement endpoint to fetch an admin's firstname, lastname, and email

--- advisor-backend/routes/admin.js
+++ advisor-backend/routes/admin.js
@@ -1,5 +1,21 @@
 import express from "express";
+import { Admin } from "../models/index.js";
 
 const router = express.Router();
+
+// Route for admin info
+router.get("/:adminId", async (req, res) => {
+  const adminId = req.params.adminId;
+
+  try {
+    const admin = await Admin.findOne({
+      where: { id: adminId },
+      attributes: ["firstname", "lastname", "email"],
+    });
+    res.status(200).json({ admin });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 export default router;
