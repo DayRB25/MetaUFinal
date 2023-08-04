ace54dab4762eb182796bf879fd827d0834d62b8
Ryan Brown <rkbrown25@meta.com>
2023-08-04 02:00:39 -0700

    Added comments and grouped imports

--- advisor-ui/src/pages/ScheduleTool/ScheduleTool.jsx
+++ advisor-ui/src/pages/ScheduleTool/ScheduleTool.jsx
@@ -1,54 +1,76 @@
-import * as React from "react";
+// css imports
+import "./ScheduleTool.css";
+// library imports
 import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+// component imports
+import { UserContext } from "../../UserContext.js";
+import Constraints from "../../components/Constraints/Constraints";
+import Modal from "../../components/Modal/Modal";
+import ScheduleDetails from "../../components/ScheduleDetails/ScheduleDetails";
+import SwapModal from "../../components/SwapModal/SwapModal.jsx";
+// mui imports
 import { Button } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { UserContext } from "../../UserContext.js";
-import "./ScheduleTool.css";
-import Constraints from "../../components/Constraints/Constraints";
-import ScheduleDetails from "../../components/ScheduleDetails/ScheduleDetails";
-import Modal from "../../components/Modal/Modal";
-import SwapModal from "../../components/SwapModal/SwapModal.jsx";
 import { CircularProgress } from "@mui/material";
+// utils imports
 import apiBase from "../../utils/apiBase.js";
 import { generateNewSchedule } from "../../utils/scheduleUtils.js";
 import { createSaveScheduleRecord } from "../../utils/savedSchedulesUtils.js";
 
 export default function ScheduleTool() {
+  // state to track constraints (grad year or classes)
   const [constraints, setConstraints] = useState([]);
+  // state to track the current schedule
   const [schedule, setSchedule] = useState([]);
+  // state to track the current schedule's adjacency list
   const [scheduleAdjList, setScheduleAdjList] = useState([]);
+  // state to track whether displayYear is engaged (when a user clicks on a year for more details)
   const [displayYear, setDisplayYear] = useState(false);
+  // state to track what year has been clicked on (for expanded view)
   const [year, setYear] = useState(null);
+  // contains user info
   const { user } = useContext(UserContext);
+  // controls open/closed nature of omdal
   const [isOpen, setIsOpen] = useState(false);
+  // tracks the course that was clicked on to initiate swap operation
   const [courseToChange, setCourseToChange] = useState({});
+  // tracks the valid swap options for a course swap into a full year
   const [options, setOptions] = useState(null);
+  // is loading state for schedule operations
   const [scheduleIsLoading, setScheduleIsLoading] = useState(false);
+  // is loading state for swap operations (passed into the modal)
   const [swapOperationIsLoading, setSwapOperationIsLoading] = useState(false);
 
+  // handler function to control opening of modal
+  // stopPropagation needed to prevent displayYear from triggering
+  // since class component is child of year component
   const handleOpenModal = (e, course) => {
     e.stopPropagation();
     setIsOpen(true);
     setCourseToChange(course);
   };
 
+  // handler function to close modal
   const handleCloseModal = () => {
     setOptions(null);
     setIsOpen(false);
   };
 
+  // handler function to set the year for display and initiate display year view
   const handleDisplayYear = (number) => {
     const yearIdx = schedule.findIndex((year) => year.number === number);
     setYear(schedule[yearIdx]);
     setDisplayYear(true);
   };
 
+  // handler function to return to regular view
   const handleCloseYear = () => {
     setDisplayYear(false);
     setYear(null);
   };
 
+  // function to ensure constraints array does not contain duplicate constraints
   const containDuplicate = (constraints, newConstraint) => {
     let flag = false;
     constraints.forEach((constraint) => {
@@ -62,6 +84,7 @@ export default function ScheduleTool() {
     return flag;
   };
 
+  // function to add constraint to constraints state
   const addConstraint = (type, value) => {
     const newConstraint = { type, value };
     if (containDuplicate(constraints, newConstraint)) {
@@ -75,6 +98,8 @@ export default function ScheduleTool() {
     }
   };
 
+  // function used to extract specifically the course constraints
+  // from the constraints array
   const isolateCourseConstraints = () => {
     const preferredCourses = constraints.filter(
       (constraint) => constraint.type === "Class"
@@ -82,6 +107,7 @@ export default function ScheduleTool() {
     return preferredCourses.map((course) => course.value);
   };
 
+  // function used to extract specifically the grad year requirement
   const isolateGradYearConstraint = () => {
     const gradYearConstraint = constraints.filter(
       (constraint) => constraint.type === "Graduation"
@@ -89,6 +115,7 @@ export default function ScheduleTool() {
     return gradYearConstraint.map((constraint) => constraint.value);
   };
 
+  // handler function to generate new schedule, calls util function
   const handleGenerateNewSchedule = async () => {
     const preferredCourses = isolateCourseConstraints();
     // return is an array
@@ -104,6 +131,7 @@ export default function ScheduleTool() {
     );
   };
 
+  // function to identify the year number for which a course resides in
   const getYearNumberFromCourse = (course) => {
     for (let i = 0; i < schedule.length; i++) {
       const year = schedule[i];
@@ -117,6 +145,7 @@ export default function ScheduleTool() {
     return -1;
   };
 
+  // function to get all years in the schedule besides the one the arg course resides in
   const extractYearsWithoutCourseYear = (course) => {
     const years = schedule
       .filter((year) => year.number != getYearNumberFromCourse(course))
@@ -124,6 +153,7 @@ export default function ScheduleTool() {
     return years;
   };
 
+  // function handling swapping into a year with less than 6 classes (not full)
   const submitNonFullYearRequest = async (desiredYear) => {
     const body = {
       schedule,
@@ -146,6 +176,8 @@ export default function ScheduleTool() {
     setSwapOperationIsLoading(false);
   };
 
+  // function handling swapping into a year with 6 classes (full)
+  // fetches a potential list of valid swap options
   const submitFullYearRequest = async (desiredYear) => {
     const body = {
       schedule,
@@ -167,6 +199,7 @@ export default function ScheduleTool() {
     setSwapOperationIsLoading(false);
   };
 
+  // handler function to identify which function to call (full or non-full)
   const handleSubmitSwapRequest = (desiredYear) => {
     // check if the year is full
     const yearIdx = schedule.findIndex((year) => year.number === desiredYear);
@@ -178,10 +211,14 @@ export default function ScheduleTool() {
     }
   };
 
+  // function to get the index in the schedule array that corresponds to the year
+  // with number that matches the arg yearNumber
   const getYearIdxFromYearNumber = (yearNumber) => {
     return schedule.findIndex((year) => year.number === yearNumber);
   };
 
+  // function to get course details (an object w/ name, description, etc)
+  // from only a course name
   const getCourseFromName = (courseName, desiredYear) => {
     const yearIdx = getYearIdxFromYearNumber(desiredYear);
     const courseIdx = schedule[yearIdx].semesters[0].classes.findIndex(
@@ -190,6 +227,8 @@ export default function ScheduleTool() {
     return schedule[yearIdx].semesters[0].classes[courseIdx];
   };
 
+  // function to handle a swap between classes (full-year swap)
+  // returns new schedule with courses swapped
   const submitCourseSwapRequest = async (course, desiredYear) => {
     const courses = [];
     const courseToSwap = getCourseFromName(course, desiredYear);
@@ -211,6 +250,7 @@ export default function ScheduleTool() {
     setSwapOperationIsLoading(false);
   };
 
+  // handler function to save schedule
   const handleSaveSchedule = async () => {
     if (schedule.length !== 0) {
       await createSaveScheduleRecord(user.id, schedule, setScheduleIsLoading);
