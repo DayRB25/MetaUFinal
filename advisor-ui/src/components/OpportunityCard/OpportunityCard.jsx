ceac5c711c788cf54b4e3ddde18dcf169ff2beea
Ryan Brown <rkbrown25@meta.com>
2023-08-03 22:29:19 -0700

    Grouping imports, adding comments for additional clarity

--- advisor-ui/src/components/OpportunityCard/OpportunityCard.jsx
+++ advisor-ui/src/components/OpportunityCard/OpportunityCard.jsx
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
-import defaultImg from "../../assets/volunteer-opportunities-ideas-article-1200x800.jpg";
+// css imports
 import "./OpportunityCard.css";
+// library imports
+import { useState } from "react";
+// component imports
 import OpportunityModal from "../OpportunityModal/OpportunityModal";
 import Modal from "../Modal/Modal";
+// mui imports
 import Chip from "@mui/material/Chip";
 import Grow from "@mui/material/Grow";
+// asset imports
+import defaultImg from "../../assets/volunteer-opportunities-ideas-article-1200x800.jpg";
 
 export default function OpportunityCard({ eventItem }) {
+  // state controlling the open/closed behavior of modal
   const [isOpen, setIsOpen] = useState(false);
-
+  // conditional styling for strength of match property returned from recommendation endpoint
   let backgroundColor = "#8BC34A";
   if (eventItem.match && eventItem.match === "medium") {
     backgroundColor = "#FFD54F";
@@ -16,10 +22,12 @@ export default function OpportunityCard({ eventItem }) {
     backgroundColor = "#E57373";
   }
 
+  // handler to set isOpen to true-- opening modal
   const handleOpenModal = () => {
     setIsOpen(true);
   };
 
+  // handle to set isOpen to false-- closing modal
   const handleCloseModal = () => {
     setIsOpen(false);
   };
